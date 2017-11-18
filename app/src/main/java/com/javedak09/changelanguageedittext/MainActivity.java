d879b1f4556a0ac9b075a85631341ef6514c6ad6
javed.khan <javedak09@gmail.com>
2017-11-18 14:12:50 +0500

    Initial Commit

---
+++ app/src/main/java/com/javedak09/changelanguageedittext/MainActivity.java
@@ -0,0 +1,17 @@
+package com.javedak09.changelanguageedittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        
+    }
+}
