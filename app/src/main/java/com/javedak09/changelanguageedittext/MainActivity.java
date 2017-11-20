3181e272271ca097ecafa6ef465bab778fb82916
javed.khan <javedak09@gmail.com>
2017-11-20 17:50:40 +0500

    Final Commit

--- app/src/main/java/com/javedak09/changelanguageedittext/MainActivity.java
+++ app/src/main/java/com/javedak09/changelanguageedittext/MainActivity.java
@@ -1,13 +1,46 @@
 package com.javedak09.changelanguageedittext;
 
 import android.app.Activity;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    protected KeyboardView keyboardView;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        InAppKeyboard keyboard = (InAppKeyboard) findViewById(R.id.keyboard);
+
+        // prevent system in_app_keyboard from appearing when EditText is tapped
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        // pass the InputConnection from the EditText to the in_app_keyboard
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
     }
 }
